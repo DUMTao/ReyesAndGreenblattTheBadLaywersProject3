882313f2b5ff9e874636170e78e62353e27b710d
DUMTao <kreyes@mocs.flsouthern.edu>
2026-04-03 14:24:44 -0400

    Silly mistakes hehehee

--- src/FriendsFinder.java
+++ src/FriendsFinder.java
@@ -13,42 +13,52 @@ public class FriendsFinder {
 
     //1. Failed 200,000 68/100
     public static List<Point> nearestFriends(List<Point> points){
+        //Creating the lists for the result of nearest friends and the points
         List<Point> nearestFriends = new ArrayList<>();
         List<Point> pointsX = new ArrayList<>(points);
         List<Point> pointsY = new ArrayList<>(points);
 
+        //Base case: If there's no points or there's only 2 then the distance is the min by default
         if (points == null || points.size() < 2){
             return nearestFriends;
         }
 
+        //Sort the points by comparing their distances
         pointsX.sort(compareX);
         pointsY.sort(compareY);
 
+        //Store the recursive result into the worker thread and save for later
         WorkerResult result = closestPairPoints(pointsX, pointsY);
 
+        //Returns the min distance pair of points
         nearestFriends.add(result.p1);
         nearestFriends.add(result.p2);
         return nearestFriends;
 
     }
 
+    /*
+    These comparators are for comparing the split distances, first sort my x and then my y
+     */
     static Comparator<Point> compareX = new Comparator<Point>() {
         public int compare(Point x1, Point x2) {
-            return Double.compare(x1.getX(), x2.getY());
+            return Double.compare(x1.getX(), x2.getX());
         }
     };
 
     static Comparator<Point> compareY = new Comparator<Point>() {
         public int compare(Point y1, Point y2) {
-            return Double.compare(y1.getX(), y2.getY());
+            return Double.compare(y1.getY(), y2.getY());
         }
     };
 
+    //Regular distance formula
     public static double distance(Point p1, Point p2) {
         double d = Math.sqrt(Math.pow((p1.getX()-p2.getX()), 2) + Math.pow((p1.getY()-p2.getY()), 2));
         return d;
     }
 
+    //Thread worker class
     static class WorkerResult {
         double distance;
         Point p1;
@@ -65,18 +75,22 @@ public class FriendsFinder {
         List<Point> leftYPoints = new ArrayList<>();
         List<Point> rightYPoints = new ArrayList<>();
         List<Point> distanceStrip = new ArrayList<>();
-        int n = pointsX.size();
+        int n = pointsX.size(); //Get how many points are within the x list
 
         //If there's less than three nodes just brute force
         if (n <= 3){
             return bruteF(pointsX);
         }
 
+        //Since there's more than 3 nodes, get the midpoint of the "graph" by the half of pointsX
         int mid = n / 2;
         Point midP = pointsX.get(mid);
 
+        //Separate and Create the lists of the x points on the left and right sides based on the midpoint
         List<Point> leftXPoints = pointsX.subList(0,  mid);
         List<Point> rightXPoints = pointsX.subList(mid, n);
+
+        //Iterate through the points to populate the left and right x points
         for (Point point : pointsY){
             if (point.getX() <= midP.getX()){
                 leftYPoints.add(point);
@@ -88,7 +102,7 @@ public class FriendsFinder {
 
         //Yay recursion! Then get the shortest distance of the closest pairs
         WorkerResult left = closestPairPoints(leftXPoints, leftYPoints);
-        WorkerResult right = closestPairPoints(leftXPoints, leftYPoints);
+        WorkerResult right = closestPairPoints(rightXPoints, rightYPoints);
 
         WorkerResult bestDistance = left.distance < right.distance ? left : right;
         double delta = bestDistance.distance;
@@ -104,8 +118,8 @@ public class FriendsFinder {
 
     }
 
-    private static double bruteF(List<Point> points) {
-        double minDis = double.MAX_VALUE;
+    private static WorkerResult bruteF(List<Point> points) {
+        double minDis = Double.MAX_VALUE;
         Point p1 = null;
         Point p2 = null;
 
@@ -119,6 +133,7 @@ public class FriendsFinder {
                 }
             }
         }
+        return new WorkerResult(minDis, p1, p2);
     }
 
     private static WorkerResult stripBest(List<Point> strip, double delta) {
